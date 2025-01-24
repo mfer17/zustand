2864825e078e8dbfbea8117d76ec0e532b8b8e09
Maria Fernanda Monoga Mejia <desarrolladorweb4@kronotime.com.co>
2025-01-23 19:33:22 -0500

    ajustar zustand con firebase, para enviar la data y quede almacenada en una realtime database

---
+++ src/stores/person/person.store.ts
@@ -0,0 +1,34 @@
+import { type StateCreator, create } from "zustand";
+import { persist } from "zustand/middleware";
+import { customSessionStorage } from "../storages/session-storage";
+import { firebaseStorage } from "../storages/firebase.storage";
+
+interface PersonState {
+    firstName: string;
+    lastName: string;
+    
+}
+
+interface Actions {
+    setFirstName: (value: string) => void;
+    setLastName: (value: string) => void;
+}
+
+const storeApi: StateCreator<PersonState & Actions> = (set) => ({
+    firstName: '',
+    lastName: '',
+    setFirstName: (value: string) => set( state => ({ firstName: value }) ),
+    setLastName: (value: string) => set( state => ({ lastName: value }) ),
+
+});
+
+
+export const usePersonStore = create<PersonState & Actions>()(
+    persist(
+        storeApi, 
+        {
+            name: 'person-storage', 
+            storage: firebaseStorage
+        }
+    )
+);
---
+++ src/stores/storages/firebase.storage.ts
@@ -0,0 +1,29 @@
+import { StateStorage, createJSONStorage } from 'zustand/middleware'
+
+const firebaseUrl = 'https://zustand-2365d-default-rtdb.firebaseio.com/zustand';
+
+const firebaseAPI: StateStorage = {
+    getItem: async function ( name: string ): Promise< string | null >  {
+        try {
+            const data = await fetch(`${firebaseUrl}/${name}.json`).then(res => res.json());
+            return JSON.stringify(data);
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    setItem: async function ( name: string, value: string ): Promise<void> {
+        const data = await fetch(`${firebaseUrl}/${name}.json`, {
+            method: 'PUT',
+            body: value
+        }).then(res => res.json());
+        return;
+    },
+
+    
+    removeItem: function ( name: string ): void | Promise<void> {
+        sessionStorage.removeItem(name);
+    }
+}
+
+export const firebaseStorage = createJSONStorage( () => firebaseAPI)
