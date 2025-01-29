5fa8a6e73fac79fd821134c7a435eaae9112c491
Maria Fernanda Monoga Mejia <desarrolladorweb4@kronotime.com.co>
2025-01-29 00:08:06 -0500

    practica con slices, como crearlos y el concepto, tambien se realiza el envio de los datos de un form

--- src/pages/03-slices/WeddingInvitationPage.tsx
+++ src/pages/03-slices/WeddingInvitationPage.tsx
@@ -1,8 +1,33 @@
+import { FormEvent, useState } from 'react';
 import { WhiteCard } from '../../components';
+import { useWeddingBoundStore } from '../../stores/wedding';
 
 
 
 export const WeddingInvitationPage = () => {
+
+  const firstName = useWeddingBoundStore( state => state.firstName );
+  const lastName = useWeddingBoundStore( state => state.lastName );
+  const guestCount = useWeddingBoundStore( state => state.guestCount );
+  const eventDate = useWeddingBoundStore( state => state.eventDate );
+  const eventYYYYMMDD = useWeddingBoundStore( state => state.eventYYYYMMDD() );
+  const eventHHMM = useWeddingBoundStore( state => state.eventHHMM() );
+  const isConfirmed = useWeddingBoundStore( state => state.isConfirmed );
+
+  const setFirstName = useWeddingBoundStore( state => state.setFirstName );
+  const setLastName = useWeddingBoundStore( state => state.setLastName );
+  const setGuestCount = useWeddingBoundStore( state => state.setGuestCount );
+  const setEventDate = useWeddingBoundStore( state => state.setEventDate );
+  const setEventTime = useWeddingBoundStore( state => state.setEventTime );
+  const setIsConfirmed = useWeddingBoundStore( state => state.setIsConfirmed );
+  
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    console.log({firstName, lastName, guestCount, eventDate, isConfirmed});
+  }
+
+
   return (
     <>
       <h1>Invitación de Boda</h1>
@@ -11,7 +36,7 @@ export const WeddingInvitationPage = () => {
 
       <WhiteCard className="flex items-center justify-center p-12">
         <div className="mx-auto w-full max-w-[550px]">
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="-mx-3 flex flex-wrap">
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
@@ -25,6 +50,8 @@ export const WeddingInvitationPage = () => {
                     name="firstName"
                     id="firstName"
                     placeholder="Primer Nombre"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </div>
               </div>
@@ -40,6 +67,8 @@ export const WeddingInvitationPage = () => {
                     name="lastName"
                     id="lastName"
                     placeholder="Apellido"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                 </div>
               </div>
@@ -57,6 +86,8 @@ export const WeddingInvitationPage = () => {
                 placeholder="5"
                 min="0"
                 className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                value={guestCount}
+                onChange={(e) => setGuestCount(parseInt(e.target.value))}
               />
             </div>
 
@@ -72,6 +103,8 @@ export const WeddingInvitationPage = () => {
                     type="date"
                     name="eventDate"
                     id="eventDate"
+                    value={eventYYYYMMDD}
+                    onChange={(e) => setEventDate(e.target.value)}
                   />
                 </div>
               </div>
@@ -86,6 +119,8 @@ export const WeddingInvitationPage = () => {
                     type="time"
                     name="eventTime"
                     id="eventTime"
+                    value={eventHHMM}
+                    onChange={(e) => setEventTime(e.target.value)}
                   />
                 </div>
               </div>
@@ -102,6 +137,8 @@ export const WeddingInvitationPage = () => {
                     name="isComing"
                     id="radioButton1"
                     className="h-5 w-5"
+                    checked={isConfirmed}
+                    onChange={() => setIsConfirmed(true)}
                   />
                   <label
                     className="pl-3 text-base font-medium text-[#07074D]"
@@ -115,6 +152,9 @@ export const WeddingInvitationPage = () => {
                     name="isComing"
                     id="radioButton2"
                     className="h-5 w-5"
+                    checked={!isConfirmed}
+                    value="NO"
+                    onChange={() => setIsConfirmed(false)}
                   />
                   <label
                     className="pl-3 text-base font-medium text-[#07074D]"
